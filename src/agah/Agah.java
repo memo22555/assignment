b85e6fbe9d83c464e56800ea02c553e5f438753b
unknown <memoyasser22555@gmail.com>
2021-02-14 22:25:31 +0200

    update

--- src/agah/Agah.java
+++ src/agah/Agah.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public class Agah {
  public static boolean bool=false ;
- public static String[] user=new String[2];
+ public static String[] user=new String[1];
  
     public static void main(String[] args) {
 
@@ -14,7 +14,7 @@ public class Agah {
      
         
         
-        user();
+       user();
         System.out.println("Enter number");   
           check(rdm.nextInt(100));
 
@@ -22,24 +22,18 @@ public class Agah {
     }
     
 public static void check(int r)
-{for (int m=0;m<2;m++){
-    
-            System.out.println(user[m]+" turn");
-   if (m==1)
-   {m=-1;
+{
    
-   }
-
-   
-    for(int i=0;true;i++)  {
+    for(int i=1;true;i++)  {
         Scanner scan=new Scanner(System.in);
                  int input=scan.nextInt();
-                 System.out.println("");
+                 System.out.println(i);
 
     if (r==input)
           {
-              System.out.println("Congratulation "+user[m]+"\n\n"); 
+              System.out.println("Congratulation "+user[0]+"\n\n"); 
               bool=true;
+              System.out.println("number of attempts for "+user[0]+" = "+i);
               break;
               
           }else
@@ -49,24 +43,24 @@ public static void check(int r)
           }
            else
                   System.out.println("please enter a smaller number");
-              break;
+             
           }
     }
     if (bool==true){
         System.out.println("*************************************");
         System.out.println("winner winner chiken Dinner");
-        break;
+        
     }
     
 
-}
+
 }
 public static void user() {
             Scanner scan=new Scanner(System.in);
-            System.out.println("enter first name");
+            System.out.println("enter name");
             user[0]=scan.next();
-            System.out.println("enter secound name");
-            user[1]=scan.next();
+           
+      
 
 }
 }
